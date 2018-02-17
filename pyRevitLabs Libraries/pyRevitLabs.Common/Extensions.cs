72cf5cf29ce32b445814f50bd3f5d71d6d40b1e1
Iran-Nejad <16073@ssoe.com>
2018-02-17 11:06:08 -0800

    Seed namespaces and some bacis functionality

---
+++ pyRevitLabs Libraries/pyRevitLabs.Common/Extensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyRevitLabs.Common.Extensions {
+    public static class FileSizeExtension {
+        public static string CleanupSize(this float bytes) {
+            var sizes = new List<String> { "Bytes", "KB", "MB", "GB", "TB" };
+
+            if (bytes == 0)
+                return "0 Byte";
+
+            var i = (int)Math.Floor(Math.Log(bytes) / Math.Log(1024));
+            if (i >= 0 && i <= sizes.Count)
+                return Math.Round(bytes / Math.Pow(1024, i), 2) + " " + sizes[i];
+            else
+                return bytes + " Bytes";
+        }
+
+        public static string CleanupSize(this long bytes) {
+            return CleanupSize((float)bytes);
+        }
+
+        public static string CleanupSize(this int bytes) {
+            return CleanupSize((float)bytes);
+        }
+    }
+
+    public static class InputExtensions {
+        public static int LimitToRange(this int value, int inclusiveMinimum, int inclusiveMaximum) {
+            if (value < inclusiveMinimum) { return inclusiveMinimum; }
+            if (value > inclusiveMaximum) { return inclusiveMaximum; }
+            return value;
+        }
+    }
+
+    public static class StringExtensions {
+        public static string GetDisplayPath(this string sourceString) {
+            var separator = Path.AltDirectorySeparatorChar.ToString();
+            return sourceString.Replace("||", separator)
+                               .Replace("|\\", separator)
+                               .Replace("|", separator)
+                               .Replace(Path.DirectorySeparatorChar.ToString(), separator);
+        }
+
+        public static string TripleDot(this string sourceString, uint maxLength) {
+            if (sourceString.Length > maxLength && maxLength > 3)
+                return sourceString.Substring(0, (int)maxLength - 3) + "...";
+            else
+                return sourceString;
+        }
+
+        public static string NullToNA(this string sourceString) {
+            if (sourceString == "" || sourceString == null)
+                return "N/A";
+            else
+                return sourceString;
+        }
+    }
+
+    public static class DateTimeExtensions {
+        public static string NeatTime(this DateTime sourceDate) {
+            return String.Format("{0:dd/MM/yyyy HH:mm:ss}", sourceDate);
+        }
+    }
+}
---
+++ pyRevitLabs Libraries/pyRevitLabs.CommonCLI/ConsoleProvider.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyRevitLabs.CommonWPF
+{
+    public class ConsoleProvider
+    {
+        private const string    KEREL32_DLLNAME = "kernel32.dll";
+        private const int       ATTACH_PARENT_PROCESS = -1;
+
+        [DllImport(KEREL32_DLLNAME)]
+        private static extern bool AttachConsole(int processId);
+
+        [DllImport(KEREL32_DLLNAME)]
+        private static extern bool AllocConsole();
+
+        [DllImport(KEREL32_DLLNAME)]
+        private static extern bool FreeConsole();
+
+        [DllImport(KEREL32_DLLNAME)]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport(KEREL32_DLLNAME)]
+        private static extern int GetConsoleOutputCP();
+
+        public static bool HasConsole
+        {
+            get { return GetConsoleWindow() != IntPtr.Zero; }
+        }
+
+        /// <summary>
+        /// Creates a new console instance if the process is not attached to a console already.
+        /// </summary>
+        public static void Show()
+        {
+            //#if DEBUG
+            if (!HasConsole)
+            {
+                AllocConsole();
+                InvalidateOutAndError();
+            }
+            //#endif
+        }
+
+        /// <summary>
+        /// If the process has a console attached to it, it will be detached and no longer visible. Writing to the System.Console is still possible, but no output will be shown.
+        /// </summary>
+        public static void Hide()
+        {
+            //#if DEBUG
+            if (HasConsole)
+            {
+                SetOutAndErrorNull();
+                FreeConsole();
+            }
+            //#endif
+        }
+
+        /// <summary>
+        /// Attach to existing console. This is helpful when running program from existing console.
+        /// </summary>
+        public static void Attach()
+        {
+            //#if DEBUG
+            if (!HasConsole)
+            {
+                AttachConsole(ATTACH_PARENT_PROCESS);
+            }
+            //#endif
+        }
+
+        /// <summary>
+        /// Detach from existing console. This is helpful when running program from existing console.
+        /// </summary>
+        public static void Detach()
+        {
+            Hide();
+        }
+
+        public static void Toggle()
+        {
+            if (HasConsole)
+            {
+                Hide();
+            }
+            else
+            {
+                Show();
+            }
+        }
+
+        static void InvalidateOutAndError()
+        {
+            Type type = typeof(System.Console);
+
+            System.Reflection.FieldInfo _out = type.GetField("_out",
+                System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            System.Reflection.FieldInfo _error = type.GetField("_error",
+                System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            System.Reflection.MethodInfo _InitializeStdOutError = type.GetMethod("InitializeStdOutError",
+                System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            Debug.Assert(_out != null);
+            Debug.Assert(_error != null);
+
+            Debug.Assert(_InitializeStdOutError != null);
+
+            _out.SetValue(null, null);
+            _error.SetValue(null, null);
+
+            _InitializeStdOutError.Invoke(null, new object[] { true });
+        }
+
+        static void SetOutAndErrorNull()
+        {
+            Console.SetOut(TextWriter.Null);
+            Console.SetError(TextWriter.Null);
+        }
+    }
+}
