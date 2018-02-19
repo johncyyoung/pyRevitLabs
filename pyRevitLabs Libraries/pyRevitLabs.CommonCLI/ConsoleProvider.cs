c22faeb9f6b0b09c4411fe214820db983df1264d
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-02-18 17:00:50 -0800

    Fixed namespace name

--- pyRevitLabs Libraries/pyRevitLabs.CommonCLI/ConsoleProvider.cs
+++ pyRevitLabs Libraries/pyRevitLabs.CommonCLI/ConsoleProvider.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace pyRevitLabs.CommonWPF
+namespace pyRevitLabs.CommonCLI
 {
     public class ConsoleProvider
     {
